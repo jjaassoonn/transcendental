e3531e8a318f047ba8bcd41d3ee415c6b5208d7e
Jujian Zhang <jujian.zhang1998@outlook.com>
2020-08-14 03:40:57 +0100

    delete unneccessary code

--- README.md
+++ README.md
@@ -52,10 +52,9 @@ The main theorem is at [e_transcendental.lean](https://github.com/jjaassoonn/tra
 theorem e_transcendental : ¬ is_algebraic ℤ e :=
 ```
 
-Here is the catch. I used the following theorems directly without prove. But all the functions used were proved to be measurable and integrable on closed interval. Once ftc is in the `mathlib` I will change ftc to its correct form and prove the relevant assumption.
+Here is the catch. I used the following theorems directly without prove. But all the functions used were proved to be measurable and integrable on closed interval.
 
 ``` lean
-axiom ftc (f: ℝ -> ℝ) (a b : ℝ) (h : b ≥ a) :  (∫ x in set.Icc a b, (deriv f) x) = f b - f a
 axiom integrate_by_part (f g : ℝ -> ℝ) (a b : ℝ) (h : b ≥ a) :
     (∫ x in set.Icc a b, (f x)*(deriv g x)) = (f b) * (g b) - (f a) * (g a) - (∫ x in set.Icc a b, (deriv f x) * (g x))
 ```
--- src/test.lean
+++ src/test.lean
@@ -1,17 +1,49 @@
+import measure_theory.set_integral
+import measure_theory.bochner_integration
 import measure_theory.interval_integral
-import measure_theory.lebesgue_measure 
+import measure_theory.lebesgue_measure
+import analysis.special_functions.exp_log
+import small_things
 
-example (f : ℝ -> ℝ) {h1 : measurable f ∧ measure_theory.integrable f} (a b : ℝ) (h : b ≥ a) (c : ℝ) 
-  (f_nonneg : ∀ x ∈ set.Icc a b, f x ≥ 0) : 0 ≤ ∫ x in a .. b, f x :=
+noncomputable theory
+open_locale classical
+open_locale big_operators
+
+notation α`[X]` := polynomial α
+
+-- F(x) = ∫ t in a .. x, f t
+-- F'(x) = f(x) for x in [a,b]
+
+private lemma ftc1 (f : ℝ -> ℝ) {hf : measurable f} {hf2 : continuous f} (a b : ℝ) (h : a ≤ b) {hf3 : measure_theory.integrable_on f (set.Icc a b)} (x0 : ℝ) (hx0 : x0 ∈ set.Icc a b) : 
+  has_deriv_at (λ (b : ℝ), ∫ (x : ℝ) in a..b, f x) (f x0) x0 :=
 begin
-  rw interval_integral.integral_of_le h,
-  apply measure_theory.integral_nonneg_of_ae,
-  apply (@measure_theory.ae_restrict_iff ℝ _ _ (set.Ioc a b) _ _).2,
-  apply measure_theory.ae_of_all,
-  intros x hx,
-  simp only [and_imp, set.mem_Ioc, pi.zero_apply, ge_iff_le, set.mem_Icc] at *,
-  refine f_nonneg x _ _,
-  linarith, linarith,
-  simp only [pi.zero_apply],
-  refine is_measurable_le measurable_zero h1.1,
+  apply interval_integral.integral_has_deriv_at_of_tendsto_ae,
+  simp only [], exact hf,
+  unfold interval_integrable,
+  split,
+  apply measure_theory.integrable_on.mono hf3,
+  intros y hy,
+  simp only [set.mem_Ioc, set.mem_Icc] at ⊢ hy hx0,
+  split, linarith, linarith,
+  apply le_refl _,
+
+  apply measure_theory.integrable_on.mono hf3,
+  intros y hy,
+  simp only [set.mem_Ioc, set.mem_Icc] at ⊢ hy hx0,
+  split, linarith, linarith,
+  apply le_refl _,
+
+  apply filter.tendsto_inf_left,
+  exact continuous.tendsto hf2 x0,
 end
+
+
+/-- # Assumption-/
+/-Theorem
+fundamental theorem of calculus and integration by part is assumed. I am waiting for them to arrive in `mathlib` and I will update this part and prove relatvent additional assumptions.
+-/
+
+axiom ftc (f: ℝ -> ℝ) (a b : ℝ) (h : b ≥ a) :  (∫ x in set.Icc a b, (deriv f) x) = f b - f a
+
+axiom integrate_by_part (f g : ℝ -> ℝ) (a b : ℝ) (h : b ≥ a) :
+    (∫ x in a..b, (f x)*(deriv g x)) = (f b) * (g b) - (f a) * (g a) - (∫ x in a..b, (deriv f x) * (g x))
